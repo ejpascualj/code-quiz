9948e1ca1705fcb925ace0e297726542bda6170a
ejpascualj <ejpascualj@gmail.com>
2021-03-31 21:33:24 -0500

    adding directory structure, html, and js

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+// Global Variables
+
+// Auxiliary Funtions
+// Timer Function
+// Score Calculator Function
+// Sort Scores Function
+
+// CODE
+// 0. Display Start Screen & button
+// 1. Start Timer & Display
+// 2. Display Questions Screen 
+// 3. Calculate Score
+// 4. Display Score and Forms
+// 5. Sort Scores
+// 6. Display Scoreboard & buttons
